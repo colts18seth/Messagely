aa879d1e24b10aaa3bffd2d3c9451148e586ddd5
Seth <colts18seth@gmail.com>
2020-07-02 21:59:49 -0400

    start messagely exercise

---
+++ models/user.js
@@ -0,0 +1,66 @@
+/** User class for message.ly */
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+
+/** User of the site. */
+class User {
+
+    /** register new user -- returns
+     *    {username, password, first_name, last_name, phone}
+     */
+    static async register({ username, password, first_name, last_name, phone }) {
+        const result = await db.query(
+            `INSERT INTO users (
+              from_username,
+              to_username,
+              body,
+              sent_at)
+            VALUES ($1, $2, $3, current_timestamp)
+            RETURNING id, from_username, to_username, body, sent_at`,
+            [from_username, to_username, body]);
+
+        return result.rows[0];
+    }
+
+    /** Authenticate: is this username/password valid? Returns boolean. */
+    static async authenticate(username, password) { }
+
+    /** Update last_login_at for user */
+    static async updateLoginTimestamp(username) { }
+
+    /** All: basic info on all users:
+     * [{username, first_name, last_name, phone}, ...] */
+    static async all() { }
+
+    /** Get: get user by username
+     *
+     * returns {username,
+     *          first_name,
+     *          last_name,
+     *          phone,
+     *          join_at,
+     *          last_login_at } */
+    static async get(username) { }
+
+    /** Return messages from this user.
+     *
+     * [{id, to_user, body, sent_at, read_at}]
+     *
+     * where to_user is
+     *   {username, first_name, last_name, phone}
+     */
+    static async messagesFrom(username) { }
+
+    /** Return messages to this user.
+     *
+     * [{id, from_user, body, sent_at, read_at}]
+     *
+     * where from_user is
+     *   {id, first_name, last_name, phone}
+     */
+    static async messagesTo(username) { }
+}
+
+
+module.exports = User;
